ec82220f30f163bdd435fdfe09477508e2773a3e
XcenaX <40425017+XcenaX@users.noreply.github.com>
2020-01-15 22:01:38 +0600

    added popular page

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.main, name='home'),
+    path('popular', views.popular, name='popular'),
     path('news/<slug:title>/', views.item, name='new'),    
 ]
--- polls/views.py
+++ polls/views.py
@@ -69,6 +69,58 @@ def main(request):
         "popular": popular,
     })  
 
+def popular(request):      
+    pub_date = request.GET.get('pub_date')              
+    title = request.GET.get('title')
+        
+    snews = New.objects.order_by("-views")  
+    if pub_date:
+        snews = snews.filter(pub_date__icontains=pub_date)
+    else:
+        pub_date = ""
+    if title:    
+        snews = snews.filter(title__icontains=title)
+    else:
+        title = ""
+    paginator = Paginator(snews, COUNT_BLOCKS_ON_PAGE)     
+    page = request.GET.get('page')        
+    try:
+        page = int(page)
+    except:
+        page = 1
+    a = ""
+    snews = ""
+    pages=[]
+    if page:
+        try:
+            snews = paginator.page(page)
+        except EmptyPage:
+            snews = paginator.page(paginator.num_pages)
+            page = paginator.num_pages
 
+        for i in range(page-2, page+3):
+            try:
+                a = paginator.page(i)
+                pages.append(i)
+            except:
+                continue        
+        if pages[-1] != paginator.num_pages:
+            pages.append(paginator.num_pages)
+
+        if pages[0] != 1:
+            pages.insert(0, 1)
+    else:
+        pages = [1,2,3,4,5,paginator.num_pages]
+        snews = paginator.page(1)
+    
+    popular = New.objects.order_by("-views")[:5]
+
+    return render(request, 'popular.html', {
+        'snews': snews,
+        'pub_date': pub_date,  
+        "title": title,     
+        "pages": pages,
+        "popular": popular,
+    })  
       
 
